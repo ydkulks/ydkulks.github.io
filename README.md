3afd5f2c57d9cacbd0ce783e92a65fd374f5d96e
ydkulks <ydkulks2@gmail.com>
2024-08-05 22:14:42 +0530

    New beginning
    Revamp of portfolio using htmx and tailwindcss

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # portfolio
+[Site](https://ydkulks.github.io)
+
+Contact me @ [*Mail*](mailto:ydkulks2@gmail.com)
+
+- page 1: Fullstack
+    - page 1.1: Game development
+- page 2: Art
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./public/**/*.{html,js}","./server/templates/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
