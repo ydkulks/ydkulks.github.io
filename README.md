e3450ea69aeb2784f174719f684bdc1478c5c096
ydkulks <ydkulks2@gmail.com>
2024-09-23 22:55:15 +0530

    Implemented Contact form
    Implemented contact form with EmailJS to send email when the form is
    submitted

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 # portfolio
-[Site](https://ydkulks.github.io)
+[ydkulks.github.io](https://ydkulks.github.io)
 
 Contact me @ [*Mail*](mailto:ydkulks2@gmail.com)
-
-- page 1: Fullstack
-    - page 1.1: Game development
-- page 2: Art
--- script.js
+++ script.js
@@ -3,8 +3,8 @@ window.onscroll = function() { makeNavbarSticky(); };
 function makeNavbarSticky() {
   const navbar = document.getElementById("navbar");
   const stickyPoint = 100; // Change this value to set the distance
-// xl:mx-12
-  if (window.pageYOffset >= stickyPoint) {
+  // xl:mx-12
+  if (window.scrollY >= stickyPoint) {
     navbar.classList.add("sticky");
     navbar.classList.remove("xl:mx-12")
   } else {
@@ -17,9 +17,9 @@ function toggleSkills() {
   const divs = document.getElementsByName("skill-hidden");
   const moreorless = document.getElementById("moreorless")
 
-  if(moreorless.innerText == "More"){
+  if (moreorless.innerText == "More") {
     moreorless.innerText = "Less"
-  }else{
+  } else {
     moreorless.innerText = "More"
   }
 
@@ -29,5 +29,32 @@ function toggleSkills() {
     } else {
       div.classList.add("skill-hidden");
     }
+  });
+}
+
+// EmailJS
+(function() {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init({
+    publicKey: "DYqO2n1G5ezSiJ3d0",
+  });
+})();
+
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var submitBtn = document.getElementById('get_in_touch')
+    submitBtn.innerText = "Sending..."
+    // emailjs.sendForm('service_g50refa', 'contact_form', this)
+    emailjs.sendForm('service_g50refa', 'template_5a1rjvi', this)
+
+      .then(() => {
+        submitBtn.innerText = "Get in Touch"
+        document.getElementById('contact-form').reset();
+        // console.log('SUCCESS!');
+      }, (error) => {
+        console.log('FAILED...', error);
+      });
+
   });
 }
